7f5c7067a482e96fe3787e07a54e21041a4d15c0
dongsu8142 <dongsu8142@naver.com>
2023-07-23 12:38:12 +0900

    feat(pkgs): changed gjs name

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  gjs = pkgs.callPackage ./pkgs/gjs { };
+  gtk-gjs = pkgs.callPackage ./pkgs/gtk-gjs { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
